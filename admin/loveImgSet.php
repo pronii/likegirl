88fb826331bc9fcfd1cd6b2ed0defa5e54c2f32c
Administrator <pronii@88.com>
2026-06-10 17:00:27 +0800

    ♻️ 移除未使用的DataTable扩展库
    - 移除buttons相关库(5个文件)
    - 移除keyTable库
    - 移除select库
    - 仅保留核心库和响应式扩展
    - 减少页面加载时间

--- admin/loveImgSet.php
+++ admin/loveImgSet.php
@@ -211,13 +211,6 @@ if (typeof $.fn.DataTable === 'undefined') {
 <script src="assets/js/vendor/dataTables.bootstrap4.js"></script>
 <script src="assets/js/vendor/dataTables.responsive.min.js"></script>
 <script src="assets/js/vendor/responsive.bootstrap4.min.js"></script>
-<script src="assets/js/vendor/dataTables.buttons.min.js"></script>
-<script src="assets/js/vendor/buttons.bootstrap4.min.js"></script>
-<script src="assets/js/vendor/buttons.html5.min.js"></script>
-<script src="assets/js/vendor/buttons.flash.min.js"></script>
-<script src="assets/js/vendor/buttons.print.min.js"></script>
-<script src="assets/js/vendor/dataTables.keyTable.min.js"></script>
-<script src="assets/js/vendor/dataTables.select.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
